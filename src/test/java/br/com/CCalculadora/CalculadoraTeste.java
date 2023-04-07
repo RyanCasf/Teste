d83b276fcfb063dfde45a9f0a00d59192b9a3250
= <ryaninternet62@gmail.com>
2023-04-07 19:14:13 -0300

    Adicionei o teste contendo Exception o esperando,

---
+++ src/test/java/br/com/CCalculadora/CalculadoraTeste.java
@@ -0,0 +1,28 @@
+package br.com.CCalculadora;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Calculadora")
+class CalculadoraTeste {
+
+    @Test @DisplayName("Não somar por ausência ser null.")
+    void somarNull() {
+        assertThrows(IllegalArgumentException.class, () -> Calculadora.somar(null));
+    }
+
+    @Test @DisplayName("Somar zero.")
+    void somarZero() {
+        assertEquals(0, Calculadora.somar(0.0));
+    }
+
+    @Test @DisplayName("Somar contendo valores null.")
+    void somarContendoNull() {
+        assertEquals(10, Calculadora.somar(null, null, 1l, 5.5d, 0.5f, null, 2, 1, null));
+    }
+}
